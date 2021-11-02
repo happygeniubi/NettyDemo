1204e1ed32780571c230ac9169febd3e923b7f3c
hzl <348313118@qq.com>
2021-11-02 10:45:41 +0800

    try,catch自动关闭流

---
+++ src/com/happygeniubi/nettydemo/io/TryCatchTest.java
@@ -0,0 +1,26 @@
+package com.happygeniubi.nettydemo.io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class TryCatchTest {
+    public static void main(String[] args) {
+
+        // 最后定义的最先关闭
+        try(FileInputStream fileInputStream = new FileInputStream("C:\\Users\\spinach\\Desktop\\草稿.txt");
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\spinach\\Desktop\\草稿(copy).txt");
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)) {
+
+            int size;
+            byte[] buf = new byte[bufferedInputStream.available()];
+            while ((size = bufferedInputStream.read(buf)) != -1) {
+                bufferedOutputStream.write(buf, 0, size);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
